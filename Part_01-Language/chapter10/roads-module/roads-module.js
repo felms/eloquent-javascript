b4428d74b985c8d64c0a2ffe0d0498680d603920
felipe <felipemoreira1@gmail.com>
2022-11-20 20:45:11 -0300

    Chapter10

---
+++ Part_01-Language/chapter10/roads-module/roads-module.js
@@ -0,0 +1,23 @@
+const { buildGraph } = require("./graph");
+
+const roads = [
+    "Alice's House-Bob's House",   "Alice's House-Cabin",
+    "Alice's House-Post Office",   "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop",          "Marketplace-Farm",
+    "Marketplace-Post Office",     "Marketplace-Shop",
+    "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+exports.roadGraph = function() {
+
+    let res = [];
+
+    roads.forEach(road => {
+        let [start, end] = road.split("-");
+        res.push([start, end]);
+    });
+
+    return buildGraph(res);
+};
